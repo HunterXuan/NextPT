795dbe09ab34c59442d050950919747f1f8d2d9d
HunterXuan <endpot@gmail.com>
2026-06-27 13:03:32 +0800

    fix: /admin/forum/categories path

--- be/server/api/admin/v1/forum_category.go
+++ be/server/api/admin/v1/forum_category.go
@@ -8,28 +8,28 @@ import (
 )
 
 type ForumCategoryCreateReq struct {
-	g.Meta `path:"/forum/node-categories" method:"post" tags:"AdminForum" summary:"创建论坛节点分类" perm:"admin:forum/category:*"`
+	g.Meta `path:"/forum/categories" method:"post" tags:"AdminForum" summary:"创建论坛节点分类" perm:"admin:forum/category:*"`
 	adminin.ForumCategoryCreateInp
 }
 
 type ForumCategoryCreateRes struct{}
 
 type ForumCategoryUpdateReq struct {
-	g.Meta `path:"/forum/node-categories/{id}" method:"patch" tags:"AdminForum" summary:"更新论坛节点分类" perm:"admin:forum/category:*"`
+	g.Meta `path:"/forum/categories/{id}" method:"patch" tags:"AdminForum" summary:"更新论坛节点分类" perm:"admin:forum/category:*"`
 	adminin.ForumCategoryUpdateInp
 }
 
 type ForumCategoryUpdateRes struct{}
 
 type ForumCategoryDeleteReq struct {
-	g.Meta `path:"/forum/node-categories/{id}" method:"delete" tags:"AdminForum" summary:"删除论坛节点分类" perm:"admin:forum/category:*"`
+	g.Meta `path:"/forum/categories/{id}" method:"delete" tags:"AdminForum" summary:"删除论坛节点分类" perm:"admin:forum/category:*"`
 	adminin.ForumCategoryDeleteInp
 }
 
 type ForumCategoryDeleteRes struct{}
 
 type ForumCategoryListReq struct {
-	g.Meta `path:"/forum/node-categories" method:"get" tags:"AdminForum" summary:"获取论坛节点分类列表" perm:"admin:forum/category:*"`
+	g.Meta `path:"/forum/categories" method:"get" tags:"AdminForum" summary:"获取论坛节点分类列表" perm:"admin:forum/category:*"`
 	adminin.ForumCategoryListInp
 }
 
